d2e2d55b05ff830053c9800c19fc30c1cb56e056
NikhilBhatt <nikhil22bhatt@gmail.com>
2026-01-13 15:13:28 +0530

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -201,7 +201,8 @@ async function loadLazy(doc) {
 
   loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
   loadFonts();
-
+  import('../tools/sidekick/aem-genai-variations.js');
+  
   sampleRUM('lazy');
 
 
